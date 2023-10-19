bca2dbc394f80267f6e181c8c12cbd14ffa68505
Braden McDorman <bmcdorman@gmail.com>
2023-10-19 07:26:57 -0700

    Additional work

--- server/src/kipr/create3/server/main.cpp
+++ server/src/kipr/create3/server/main.cpp
@@ -1,6 +1,31 @@
 #include <stdlib.h>
+#include <rclcpp/rclcpp.hpp>
+
+class Create3 : public rclcpp::Node
+{
+public:
+  MinimalPublisher()
+    : Node("minimal_publisher")
+  {
+    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+    timer_ = this->create_wall_timer(
+      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+  }
+};
+
 
 int main(int argc, char *const argv[])
 {
+  rclcpp::init(argc, argv);
+  
+  auto node = std::make_shared<MinimalPublisher>();
+
+  while (rclcpp::ok())
+  {
+    rclcpp::spin_some(node);
+  }
+
+  rclcpp::shutdown();
+
   return EXIT_SUCCESS;
 }
