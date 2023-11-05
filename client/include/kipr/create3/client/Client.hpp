cfbce99fbfe88f27aec7b0c8a618d7017e8479f5
Tim Corbly <tcorbly@kipr.org>
2023-11-04 20:55:43 -0600

    Revert "Initial work on delayed calls"
    This reverts commit d98604542645785ecac3d144f1e9460265864cda.

--- client/include/kipr/create3/client/Client.hpp
+++ client/include/kipr/create3/client/Client.hpp
@@ -39,61 +39,57 @@ namespace client
 
   class Client
   {
-  public:
-    friend struct ClientDebouncer;
+    public:
+      Client(std::unique_ptr<ClientImpl> &&impl);
+      Client(Client &&rhs) = default;
+      Client(const std::string &host = "localhost", const std::uint16_t port = 50051);
+      ~Client();
 
-    Client(std::unique_ptr<ClientImpl> &&impl);
-    Client(Client &&rhs) = default;
-    Client(const std::string &host = "localhost", const std::uint16_t port = 50051);
-    ~Client();
+      bool isConnected();
+      bool isDocked();
 
-    bool isConnected();
-    bool isDocked();
+      void wait();
+      void executeNextCommandImmediately();
 
-    void wait();
-    void executeNextCommandImmediately();
+      void setVelocity(const Twist &velocity);
+      void playAudio(const AudioNote *const notes, const std::size_t count, const bool overwrite = false);
 
-    void setVelocity(const Twist &velocity);
-    void playAudio(const AudioNote *const notes, const std::size_t count, const bool overwrite = false);
+      void dock();
+      void undock();
 
-    void dock();
-    void undock();
+      void driveDistance(const float distance, const float max_linear_speed);
+      void driveArc(const Direction direction, const float radius, const float angle, const float max_linear_speed);
+      void ledAnimation(const LedAnimationType animation_type, const Lightring lightring, Duration duration);
+      void lightring(const Lightring lightring);
+      void rotate(const float angle, const float max_angular_speed);
+      void navigateTo(const Pose &pose, const float max_linear_speed, const float max_angular_speed, const bool achieve_goal_heading = true);
+      void navigateTo(const double x, const double y, const double theta, const float max_linear_speed, const float max_angular_speed);
+      void navigateTo(const double x, const double y, const float max_linear_speed, const float max_angular_speed);
+      void followWall(const Follow follow, const float max_seconds);
 
-    void driveDistance(const float distance, const float max_linear_speed);
-    void driveArc(const Direction direction, const float radius, const float angle, const float max_linear_speed);
-    void ledAnimation(const LedAnimationType animation_type, const Lightring lightring, Duration duration);
-    void lightring(const Lightring lightring);
-    void rotate(const float angle, const float max_angular_speed);
-    void navigateTo(const Pose &pose, const float max_linear_speed, const float max_angular_speed, const bool achieve_goal_heading = true);
-    void navigateTo(const double x, const double y, const double theta, const float max_linear_speed, const float max_angular_speed);
-    void navigateTo(const double x, const double y, const float max_linear_speed, const float max_angular_speed);
-    void followWall(const Follow follow, const float max_seconds);
+      HazardDetectionVector getHazardDetectionVector() const;
+      IrIntensityVector getCliffIntensityVector() const;
+      IrIntensityVector getIrIntensityVector() const;
+      Odometry getOdometry() const;
 
-    HazardDetectionVector getHazardDetectionVector() const;
-    IrIntensityVector getCliffIntensityVector() const;
-    IrIntensityVector getIrIntensityVector() const;
-    Odometry getOdometry() const;
+    private:
+      Client(const Client &) = delete;
 
-  private:
-    Client(const Client &) = delete;
+      std::unique_ptr<ClientImpl> impl_;
 
-    void setVelocity_(const Twist &velocity);
+      mutable std::mutex wait_mut_;
 
-    std::unique_ptr<ClientImpl> impl_;
+      mutable std::mutex latest_cliff_intensity_vector_mut_;
+      mutable std::optional<Stamped<IrIntensityVector>> latest_cliff_intensity_vector_;
 
-    mutable std::mutex wait_mut_;
+      mutable std::mutex latest_hazard_detection_vector_mut_;
+      mutable std::optional<Stamped<HazardDetectionVector>> latest_hazard_detection_vector_;
 
-    mutable std::mutex latest_cliff_intensity_vector_mut_;
-    mutable std::optional<Stamped<IrIntensityVector>> latest_cliff_intensity_vector_;
+      mutable std::mutex latest_ir_intensity_vector_mut_;
+      mutable std::optional<Stamped<IrIntensityVector>> latest_ir_intensity_vector_;
 
-    mutable std::mutex latest_hazard_detection_vector_mut_;
-    mutable std::optional<Stamped<HazardDetectionVector>> latest_hazard_detection_vector_;
-
-    mutable std::mutex latest_ir_intensity_vector_mut_;
-    mutable std::optional<Stamped<IrIntensityVector>> latest_ir_intensity_vector_;
-
-    mutable std::mutex latest_odometry_mut_;
-    mutable std::optional<Stamped<Odometry>> latest_odometry_;
+      mutable std::mutex latest_odometry_mut_;
+      mutable std::optional<Stamped<Odometry>> latest_odometry_;
   };
 }
 }
